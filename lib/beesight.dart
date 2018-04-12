9fe7159c2aa064d1625aba5a247ebd7adaa12342
Nhan Cao <nhancv92@gmail.com>
2018-04-12 16:34:31 +0700

    beesight draw

--- lib/beesight.dart
+++ lib/beesight.dart
@@ -56,11 +56,11 @@ class _DemoBodyState extends State<DemoBody> with TickerProviderStateMixin {
 
     animationController.addStatusListener((status) {
       if (status == AnimationStatus.completed) {
-        characterFactory.step++;
-        animationController.reverse();
-      } else if (status == AnimationStatus.dismissed) {
-        characterFactory.step++;
-        animationController.forward();
+        if (characterFactory.step != -1) {
+          characterFactory.step++;
+          animationController.reset();
+          animationController.forward();
+        }
       }
     });
     animationController.forward();
@@ -83,10 +83,8 @@ class _DemoBodyState extends State<DemoBody> with TickerProviderStateMixin {
         ),
         builder: (context, child) => new CustomPaint(
               size: widget.screenSize,
-              painter: new _DemoPainter(
-                widget.screenSize,
-                characterFactory.offsetPoints,
-              ),
+              painter: new _DemoPainter(widget.screenSize,
+                  characterFactory.offsetPoint, characterFactory.path),
             ),
       ),
     );
@@ -95,22 +93,25 @@ class _DemoBodyState extends State<DemoBody> with TickerProviderStateMixin {
 
 class _DemoPainter extends CustomPainter {
   final Size screenSize;
-  final List<Offset> offsetPoints;
+  final Offset offsetPoint;
+  final Path path;
   Paint painter;
 
-  _DemoPainter(this.screenSize, this.offsetPoints) {
+  _DemoPainter(this.screenSize, this.offsetPoint, this.path) {
     painter = new Paint()
       ..strokeWidth = 1.0
       ..color = Colors.red
-      ..style = PaintingStyle.stroke;
+      ..style = PaintingStyle.fill;
   }
 
   @override
   void paint(Canvas canvas, Size size) {
-    canvas.drawPoints(ui.PointMode.polygon, offsetPoints, painter);
-    if (offsetPoints.length > 0) {
-      canvas.drawCircle(offsetPoints[offsetPoints.length - 1], 10.0, painter);
-    }
+    canvas.drawPath(path, painter);
+    canvas.drawCircle(offsetPoint, 10.0, painter);
+
+    canvas.drawLine(new Offset(0.0, 100.0), new Offset(600.0, 100.0), painter);
+    canvas.drawLine(new Offset(0.0, 150.0), new Offset(600.0, 150.0), painter);
+    canvas.drawLine(new Offset(0.0, 200.0), new Offset(600.0, 200.0), painter);
   }
 
   @override
@@ -118,7 +119,8 @@ class _DemoPainter extends CustomPainter {
 }
 
 class CharacterFactory {
-  List<Offset> offsetPoints = [];
+  Offset offsetPoint;
+  Path path = new Path();
   int step = 0;
 
   Offset getQuadraticBezier(List<Offset> offsetList, double t) {
@@ -136,29 +138,243 @@ class CharacterFactory {
   }
 
   void addPoint(double time) {
-    if (step == 0) {
-      offsetPoints.add(bCharStep1(time));
-    } else if (step == 1) {
-      offsetPoints.add(bCharStep2(time));
-    } else {
-      step = 0;
-      offsetPoints.clear();
+    bool finish = false;
+    bChar(time, 0.0, 0);
+    eChar(time, 50.0, 2);
+    eChar(time, 100.0, 4);
+    sChar(time, 150.0, 6);
+    iChar(time, 185.0, 7);
+    gChar(time, 230.0, 10);
+    hChar(time, 290.0, 13);
+    tChar(time, 330.0, 15);
+    sChar(time, 380.0, 18);
+    oChar(time, 430.0, 19);
+    fChar(time, 490.0, 20);
+    finish = tChar(time, 520.0, 22);
+
+    if (finish) {
+      step = -1;
     }
   }
 
-  Offset bCharStep1(double time) {
-    return getQuadraticBezier([
-      new Offset(0.0, 100.0),
-      new Offset(0.0, 200.0),
-    ], time);
+  bool bChar(double time, [double xOffset = 0.0, int stepOffset = 0]) {
+    if (step == stepOffset) {
+      path.moveTo(0.0 + xOffset, 100.0);
+      offsetPoint = getQuadraticBezier([
+        new Offset(0.0 + xOffset, 100.0),
+        new Offset(0.0 + xOffset, 200.0),
+      ], time);
+    } else if (step == stepOffset + 1) {
+      path.moveTo(0.0 + xOffset, 175.0);
+      offsetPoint = getQuadraticBezier([
+        new Offset(0.0 + xOffset, 175.0),
+        new Offset(35.0 + xOffset, 100.0),
+        new Offset(80.0 + xOffset, 212.0),
+        new Offset(0.0 + xOffset, 200.0),
+      ], time);
+    } else if (step > stepOffset + 1) {
+      return true;
+    }
+    path.addRect(new Rect.fromCircle(center: offsetPoint, radius: 2.0));
+    return false;
   }
 
-  Offset bCharStep2(double time) {
-    return getQuadraticBezier([
-      new Offset(0.0, 175.0),
-      new Offset(35.0, 112.0),
-      new Offset(100.0, 212.0),
-      new Offset(0.0, 200.0),
-    ], time);
+  bool eChar(double time, [double xOffset = 0.0, int stepOffset = 0]) {
+    if (step == stepOffset) {
+      path.moveTo(0.0 + xOffset, 185.0);
+      offsetPoint = getQuadraticBezier([
+        new Offset(0.0 + xOffset, 185.0),
+        new Offset(60.0 + xOffset, 165.0),
+        new Offset(45.0 + xOffset, 150.0),
+        new Offset(25.0 + xOffset, 150.0),
+      ], time);
+    } else if (step == stepOffset + 1) {
+      path.moveTo(25.0 + xOffset, 150.0);
+      offsetPoint = getQuadraticBezier([
+        new Offset(25.0 + xOffset, 150.0),
+        new Offset(0.0 + xOffset, 155.0),
+        new Offset(-5.0 + xOffset, 200.0),
+        new Offset(5.0 + xOffset, 215.0),
+        new Offset(40.0 + xOffset, 185.0),
+      ], time);
+    } else if (step > stepOffset + 1) {
+      return true;
+    }
+    path.addRect(new Rect.fromCircle(center: offsetPoint, radius: 2.0));
+    return false;
   }
+
+  bool sChar(double time, [double xOffset = 0.0, int stepOffset = 0]) {
+    if (step == stepOffset) {
+      path.moveTo(40.0 + xOffset, 165.0);
+      offsetPoint = getQuadraticBezier([
+        new Offset(40.0 + xOffset, 165.0),
+        new Offset(40.0 + xOffset, 135.0),
+        new Offset(0.0 + xOffset, 150.0),
+        new Offset(-20.0 + xOffset, 155.0),
+        new Offset(-20.0 + xOffset, 175.0),
+        new Offset(100.0 + xOffset, 180.0),
+        new Offset(50.0 + xOffset, 185.0),
+        new Offset(50.0 + xOffset, 215.0),
+        new Offset(25.0 + xOffset, 205.0),
+        new Offset(15.0 + xOffset, 200.0),
+        new Offset(0.0 + xOffset, 185.0),
+      ], time);
+    } else if (step > stepOffset) {
+      return true;
+    }
+    path.addRect(new Rect.fromCircle(center: offsetPoint, radius: 2.0));
+    return false;
+  }
+
+  bool iChar(double time, [double xOffset = 0.0, int stepOffset = 0]) {
+    if (step == stepOffset) {
+      path.moveTo(20.0 + xOffset, 150.0);
+      offsetPoint = getQuadraticBezier([
+        new Offset(20.0 + xOffset, 150.0),
+        new Offset(20.0 + xOffset, 195.0),
+      ], time);
+    } else if (step == stepOffset + 1) {
+      path.moveTo(20.0 + xOffset, 195.0);
+      offsetPoint = getQuadraticBezier([
+        new Offset(20.0 + xOffset, 195.0),
+        new Offset(25.0 + xOffset, 205.0),
+        new Offset(35.0 + xOffset, 195.0),
+      ], time);
+    } else if (step == stepOffset + 2) {
+      path.moveTo(20.0 + xOffset, 135.0);
+      offsetPoint = getQuadraticBezier([
+        new Offset(20.0 + xOffset, 135.0),
+        new Offset(20.0 + xOffset, 140.0),
+      ], time);
+    } else if (step > stepOffset +2) {
+      return true;
+    }
+    path.addRect(new Rect.fromCircle(center: offsetPoint, radius: 2.0));
+    return false;
+  }
+
+  bool gChar(double time, [double xOffset = 0.0, int stepOffset = 0]) {
+    if (step == stepOffset) {
+      path.moveTo(40.0 + xOffset, 150.0);
+      offsetPoint = getQuadraticBezier([
+        new Offset(40.0 + xOffset, 150.0),
+        new Offset(-5.0 + xOffset, 150.0),
+        new Offset(0.0 + xOffset, 200.0),
+        new Offset(0.0 + xOffset, 215.0),
+        new Offset(20.0 + xOffset, 200.0),
+        new Offset(40.0 + xOffset, 180.0),
+      ], time);
+    } else if (step == stepOffset + 1) {
+      path.moveTo(40.0 + xOffset, 150.0);
+      offsetPoint = getQuadraticBezier([
+        new Offset(40.0 + xOffset, 150.0),
+        new Offset(40.0 + xOffset, 240.0),
+      ], time);
+    } else if (step == stepOffset + 2) {
+      path.moveTo(40.0 + xOffset, 240.0);
+      offsetPoint = getQuadraticBezier([
+        new Offset(40.0 + xOffset, 240.0),
+        new Offset(35.0 + xOffset, 285.0),
+        new Offset(10.0 + xOffset, 285.0),
+        new Offset(-15.0 + xOffset, 250.0),
+        new Offset(45.0 + xOffset, 215.0),
+      ], time);
+    } else if (step > stepOffset + 2) {
+      return true;
+    }
+    path.addRect(new Rect.fromCircle(center: offsetPoint, radius: 2.0));
+    return false;
+  }
+
+  bool hChar(double time, [double xOffset = 0.0, int stepOffset = 0]) {
+    if (step == stepOffset) {
+      path.moveTo(0.0 + xOffset, 100.0);
+      offsetPoint = getQuadraticBezier([
+        new Offset(0.0 + xOffset, 100.0),
+        new Offset(0.0 + xOffset, 200.0),
+      ], time);
+    } else if (step == stepOffset + 1) {
+      path.moveTo(0.0 + xOffset, 175.0);
+      offsetPoint = getQuadraticBezier([
+        new Offset(0.0 + xOffset, 175.0),
+        new Offset(45.0 + xOffset, 120.0),
+        new Offset(45.0 + xOffset, 200.0),
+      ], time);
+    } else if (step > stepOffset + 1) {
+      return true;
+    }
+    path.addRect(new Rect.fromCircle(center: offsetPoint, radius: 2.0));
+    return false;
+  }
+
+  bool tChar(double time, [double xOffset = 0.0, int stepOffset = 0]) {
+    if (step == stepOffset) {
+      path.moveTo(25.0 + xOffset, 100.0);
+      offsetPoint = getQuadraticBezier([
+        new Offset(25.0 + xOffset, 100.0),
+        new Offset(25.0 + xOffset, 195.0),
+      ], time);
+    } else if (step == stepOffset + 1) {
+      path.moveTo(25.0 + xOffset, 195.0);
+      offsetPoint = getQuadraticBezier([
+        new Offset(25.0 + xOffset, 195.0),
+        new Offset(30.0 + xOffset, 205.0),
+        new Offset(45.0 + xOffset, 200.0),
+      ], time);
+    } else if (step == stepOffset + 2) {
+      path.moveTo(10.0 + xOffset, 145.0);
+      offsetPoint = getQuadraticBezier([
+        new Offset(10.0 + xOffset, 145.0),
+        new Offset(25.0 + xOffset, 150.0),
+        new Offset(45.0 + xOffset, 145.0),
+      ], time);
+    } else if (step > stepOffset + 2) {
+      return true;
+    }
+    path.addRect(new Rect.fromCircle(center: offsetPoint, radius: 2.0));
+    return false;
+  }
+
+  bool oChar(double time, [double xOffset = 0.0, int stepOffset = 0]) {
+    if (step == stepOffset) {
+      path.moveTo(25.0 + xOffset, 150.0);
+      offsetPoint = getQuadraticBezier([
+        new Offset(25.0 + xOffset, 150.0),
+        new Offset(-5.0 + xOffset, 175.0),
+        new Offset(-5.0 + xOffset, 200.0),
+        new Offset(25.0 + xOffset, 235.0),
+        new Offset(65.0 + xOffset, 175.0),
+        new Offset(65.0 + xOffset, 150.0),
+        new Offset(25.0 + xOffset, 150.0),
+      ], time);
+    } else if (step > stepOffset) {
+      return true;
+    }
+    path.addRect(new Rect.fromCircle(center: offsetPoint, radius: 2.0));
+    return false;
+  }
+
+  bool fChar(double time, [double xOffset = 0.0, int stepOffset = 0]) {
+    if (step == stepOffset) {
+      path.moveTo(40.0 + xOffset, 100.0);
+      offsetPoint = getQuadraticBezier([
+        new Offset(40.0 + xOffset, 100.0),
+        new Offset(-15.0 + xOffset, 100.0),
+        new Offset(50.0 + xOffset, 200.0),
+        new Offset(0.0 + xOffset, 200.0),
+      ], time);
+    } else if (step == stepOffset + 1) {
+      path.moveTo(0.0 + xOffset, 140.0);
+      offsetPoint = getQuadraticBezier([
+        new Offset(0.0 + xOffset, 140.0),
+        new Offset(30.0 + xOffset, 125.0),
+      ], time);
+    } else if (step > stepOffset + 1) {
+      return true;
+    }
+    path.addRect(new Rect.fromCircle(center: offsetPoint, radius: 2.0));
+    return false;
+  }
+
 }
