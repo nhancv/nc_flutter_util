9cacd79d22a06fcf1bd5138186dc1a0207e907ae
Nhan Cao <nhancv92@gmail.com>
2018-04-20 10:40:31 +0700

    wave animation clipper

--- lib/demo5.dart
+++ lib/demo5.dart
@@ -24,13 +24,14 @@ class _DemoPageState extends State<DemoPage> {
         children: <Widget>[
           new DemoBody(
             screenSize: MediaQuery.of(context).size,
-            offset: 0,
-            color: Colors.black,
+            xOffset: 0,
+            yOffset: 0,
           ),
           new DemoBody(
-              screenSize: MediaQuery.of(context).size,
-              offset: 50,
-              color: Colors.red),
+            screenSize: MediaQuery.of(context).size,
+            xOffset: 50,
+            yOffset: 10,
+          ),
         ],
       ),
     );
@@ -39,10 +40,10 @@ class _DemoPageState extends State<DemoPage> {
 
 class DemoBody extends StatefulWidget {
   final Size screenSize;
-  final int offset;
-  final Color color;
+  final int xOffset;
+  final int yOffset;
 
-  DemoBody({Key key, @required this.screenSize, this.offset, this.color})
+  DemoBody({Key key, @required this.screenSize, this.xOffset, this.yOffset})
       : super(key: key);
 
   @override
@@ -53,43 +54,29 @@ class DemoBody extends StatefulWidget {
 
 class _DemoBodyState extends State<DemoBody> with TickerProviderStateMixin {
   AnimationController animationController;
-  List<double> waveList = [];
   List<Offset> animList1 = [];
-  List<Offset> animList2 = [];
-  int step = 0;
 
   @override
   void initState() {
     super.initState();
 
-    for (int i = 0; i < 360; i++) {
-      waveList.add(sin(i * Vector.degrees2Radians) * 20 + 50);
-    }
-
-    for (int i = 0; i < 360 * 2; i++) {
-      animList1.add(new Offset(i.toDouble(), waveList[(i) % 360]));
-      animList2.add(new Offset(i.toDouble(), waveList[(i) % 360]));
-    }
-
     animationController = new AnimationController(
         vsync: this, duration: new Duration(seconds: 2));
+
     animationController.addListener(() {
-      step = (animationController.value * 720).toInt();
-//      step ++ ;
-//      print(step);
-
-      for (int i = 0; i < 360 * 2; i++) {
-        animList1[i] = new Offset(
-            animList1[i].dx, waveList[(i + step) % 360]);
-
-//        animList2[i] = new Offset(
-//            animList2[i].dx,
-//            waveList[
-//                (i + widget.offset + (360 * animationController.value).toInt()) % 360]);
+      animList1.clear();
+      for (int i = -2 - widget.xOffset;
+          i <= widget.screenSize.width.toInt() + 2;
+          i++) {
+        animList1.add(new Offset(
+            i.toDouble() + widget.xOffset,
+            sin((animationController.value * 360 - i) %
+                        360 *
+                        Vector.degrees2Radians) *
+                    20 +
+                50 + widget.yOffset));
       }
-//      if(step >= 720) step = 0;
     });
-
     animationController.repeat();
   }
 
@@ -113,14 +100,15 @@ class _DemoBodyState extends State<DemoBody> with TickerProviderStateMixin {
                 widget.screenSize.width,
                 widget.screenSize.height,
               ),
-              foregroundPainter: new _DemoPainter(widget.screenSize,
-                  animationController.value, animList1, animList2),
+              foregroundPainter: new _DemoPainter(
+                  widget.screenSize, animationController.value, animList1),
               child: new ClipPath(
-                child: new Container(
-                  width: widget.screenSize.width,
-                  height: 200.0,
-                  color: widget.color,
-                ),
+                child: Image.asset('images/demo5bg.jpg'),
+//                new Container(
+//                  width: widget.screenSize.width,
+//                  height: 200.0,
+//                  color: Colors.red,
+//                ),
                 clipper: new WaveClipper(animationController.value, animList1),
               ),
             ),
@@ -136,14 +124,12 @@ class _DemoPainter extends CustomPainter {
     ..style = PaintingStyle.stroke
     ..color = Colors.black;
   List<Offset> waveList1 = [];
-  List<Offset> waveList2 = [];
 
-  _DemoPainter(this.screenSize, this.animation, this.waveList1, this.waveList2);
+  _DemoPainter(this.screenSize, this.animation, this.waveList1);
 
   @override
   void paint(Canvas canvas, Size size) {
     canvas.drawPoints(ui.PointMode.polygon, waveList1, painter);
-//    canvas.drawPoints(ui.PointMode.polygon, waveList2, painter);
   }
 
   @override
@@ -156,20 +142,6 @@ class WaveClipper extends CustomClipper<Path> {
   List<Offset> waveList1 = [];
 
   WaveClipper(this.animation, this.waveList1);
-
-  Offset getQuadraticBezier(List<Offset> offsetList, double t) {
-    return getQuadraticBezier2(offsetList, t, 0, offsetList.length - 1);
-  }
-
-  Offset getQuadraticBezier2(List<Offset> offsetList, double t, int i, int j) {
-    if (i == j) return offsetList[i];
-
-    Offset b0 = getQuadraticBezier2(offsetList, t, i, j - 1);
-    Offset b1 = getQuadraticBezier2(offsetList, t, i + 1, j);
-    Offset res =
-        new Offset((1 - t) * b0.dx + t * b1.dx, (1 - t) * b0.dy + t * b1.dy);
-    return res;
-  }
 
   @override
   Path getClip(Size size) {
