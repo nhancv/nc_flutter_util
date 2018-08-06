18d2f3c126ae5ab701b85dcbca6c9c0979a533f1
Nhan Cao <nhancv92@gmail.com>
2018-08-06 16:13:02 +0700

    add custom view template

--- lib/main.dart
+++ lib/main.dart
@@ -5,9 +5,10 @@ import 'package:flutter/material.dart';
 //import 'demo2.dart';
 //import 'demo3.dart';
 //import 'demo4.dart';
-import 'demo5.dart';
+//import 'demo5.dart';
 //import 'demo6.dart';
 //import 'beesight.dart';
+import 'leave_picker.dart';
 
 void main() => runApp(new MyApp());
 
